eca3f8160f8325c735c7ba56c79d8eedd2b958ac
Andrew Chan <akhai@syn-2600-6c50-637f-d0b7-0000-0000-0000-1174.biz6.spectrum.com>
2025-01-02 11:41:21 -0800

    fixed pin container and added new project

--- Components/ui/PinContainer.tsx
+++ Components/ui/PinContainer.tsx
@@ -74,9 +74,9 @@ export const PinPerspective = ({
           <a
             href={href}
             target="_blank"
-            className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 "
+            className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 inline-flex min-w-[fit-content] max-w-max"
           >
-            <span className="relative z-20 text-white text-xs font-bold inline-block py-0.5">
+            <span className="relative z-20 text-white text-xs font-bold inline-block py-0.5 whitespace-nowrap">
               {title}
             </span>
 
